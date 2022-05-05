e3382a3a56153372c57c133a1f024cc8bbef4472
diegoosvaldo <diego.cruz@cmpc.cl>
2022-05-05 10:37:11 -0400

    update settings

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -101,7 +101,7 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
